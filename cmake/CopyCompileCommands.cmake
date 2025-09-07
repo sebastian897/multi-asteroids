a926742481eb9c6c64cec32057b7e373251a5b52
Sebastian Schonrock <sebastian@schonrocks.com>
2025-09-07 20:26:49 +0100

    cc.json that works

--- cmake/CopyCompileCommands.cmake
+++
@@ -1,5 +0,0 @@
-file(COPY "${CMAKE_BINARY_DIR}/compile_commands.json"
-     DESTINATION "${CMAKE_SOURCE_DIR}"
-     FILES_MATCHING PATTERN "compile_commands.json")
-
-message(STATUS "Copied compile_commands.json to project root")
